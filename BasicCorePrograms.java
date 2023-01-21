b856ce05003efe89e994cf43bf7c2fb77404d082
Akanksha-H <aakanksha0499@gmail.com>
2023-01-21 22:00:14 +0530

    Welcome to java basic programs

---
+++ BasicCorePrograms.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.day5;
+
+import java.util.Scanner;
+
+public class BasicCorePrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java basic programs");
+    }
+}
